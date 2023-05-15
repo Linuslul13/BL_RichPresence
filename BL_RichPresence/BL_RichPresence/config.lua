d3982ce0f2c2c3b1dbc9be557b4a934f1c88a67f
Linus <97615041+Linuslul13@users.noreply.github.com>
2023-05-15 11:27:10 +0200

    Update.lua Add
    - Add Update.lua

---
+++ BL_RichPresence/BL_RichPresence/config.lua
@@ -0,0 +1,3 @@
+Config = {}
+Config.ClientID = Hier kommt die Client ID rein 
+
---
+++ BL_RichPresence/BL_RichPresence/fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+games { 'gta5'}
+
+
+author 'linuslul13'
+version '1.0.1'
+description 'Simple Discord RichPresence'
+
+client_scripts {
+    'discord.lua',
+    'config.lua',
+    'update.lua'
+}
+
+server_scripts {
+    'update.lua',
+}
